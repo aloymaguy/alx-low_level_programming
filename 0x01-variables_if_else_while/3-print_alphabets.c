b483696aa9a2bfa3d0a73d8d3976d24d83476f59
CHINANU ALOYSIUS UWALAKA <88582352+aloymaguy@users.noreply.github.com>
2022-09-11 18:15:24 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,36 +1,21 @@
-**/                                                                                                                                     
+#include <stdio.h>
 
-                                                                                                                                        
+/**
+ * main - Prints the alphabet in lowercase, and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
 
-* main - Prints the alphabet in lowercase, and then in uppercase.                                                                       
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
 
-*                                                                                                                                       
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
 
-* Return: Always 0.                                                                                                                     
+	putchar('\n');
 
-*/                                                                                                                                      
-
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-        char letter;                                                                                                                    
-
-                                                                                                                                        
-
-        for (letter = 'a'; letter <= 'z'; letter++)                                                                                     
-
-                putchar(letter);                                                                                                        
-
-        for (letter = 'A'; letter <= 'Z'; letter++)                                                                                     
-
-                putchar(letter);                                                                                                        
-
-                                                                                                                                        
-
-                putchar('\n');                                                                                                          
-
-                                                                                                                                        
-
-                return (0);                                                                                                             
-
+	return (0);
+}
