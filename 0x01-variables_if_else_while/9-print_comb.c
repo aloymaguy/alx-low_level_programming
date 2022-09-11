6a3412dcb24480b64f0529d26ba501fc6fb3e1a7
CHINANU ALOYSIUS UWALAKA <88582352+aloymaguy@users.noreply.github.com>
2022-09-11 19:13:22 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Prints all possible combinations of single-digit numbers.
- *
+ * main - Printing numbers form 0-9 with commas and space between them.
+ *Description: using the main function
+ *this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
  * Return: Always 0.
  */
 int main(void)
