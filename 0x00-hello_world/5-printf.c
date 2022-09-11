510508fc720799600fb14195edc0905c65ba4dca
aloymaguy <aloymaguy601@gmail.com>
2022-09-11 06:02:43 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,26 +1,14 @@
-#include <stdio.h>                                                                                                                      
+#include <stdio.h>
 
-                                                                                                                                        
-
-/**                                                                                                                                     
-
-* main - print the string in the put function                                                                                           
-
-*                                                                                                                                       
-
-*Description: using the main function                                                                                                   
-
-*this program prints "Programming is like building a multilingual puzzle                                                                
-
-*Return: 0                                                                                                                              
-
-*/                                                                                                                                      
-
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-        print("with proper grammer, but the outcome is a piece of art,\n");                                                             
-
-        return (0);                                                                                                                     
-
+/**
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+*Resturn: 0
+*/
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of are,\n");
+	return (0);
+}
