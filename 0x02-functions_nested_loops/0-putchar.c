862d2604294def26cdf06de074e62591f084ecc1
aloymaguy <aloymaguy601@gmail.com>
2022-09-14 11:17:26 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,19 @@
-#include "main.h"
+#include "_putcher.h"
+
+/**
+*main - function to print
+*
+* Return: 0
+*/
+int main(void)
+{
+	char _putcher[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+	unasigned int c;
+
+	for (c = 0; c < sizeof(_putcher); c++)
+	{
+		_putchar(_putchar[c]);
+	}
+_putchar('\n');
+return (0);
+}
